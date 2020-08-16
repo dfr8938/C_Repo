b6e896551e1a48868ddd24527830a3bbd9489ddd
Denis Frolov <d.fr8938@gmail.com>
2020-08-16 05:25:45 +0300

    add ft_print_alphabet

---
+++ Practice/C_00/ex01/ft_h.h
@@ -0,0 +1,8 @@
+#ifndef FT_H
+# define FT_H
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_print_alphabet(void);
+#endif
---
+++ Practice/C_00/ex01/ft_print_alphabet.c
@@ -0,0 +1,13 @@
+#include "ft_h.h"
+
+void	ft_print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		ft_putchar(a);
+		a++;
+	}
+}
