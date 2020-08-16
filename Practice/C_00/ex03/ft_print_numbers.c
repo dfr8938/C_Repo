eb5e70a142385948d0ee7b63539ba580570585ec
Denis Frolov <d.fr8938@gmail.com>
2020-08-16 14:42:44 +0300

    add ft_print_numbers

---
+++ Practice/C_00/ex03/ft_print_numbers.c
@@ -0,0 +1,13 @@
+#include "ft_h.h"
+
+void	ft_print_numbers(void)
+{
+	int	i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		ft_putchar(i);
+		i++;
+	}
+}
---
+++ Practice/C_00/ex03/ft_putchar.c
@@ -0,0 +1,6 @@
+#include "ft_h.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
