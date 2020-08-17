e2532790722896be6f1bca54d4575477fe8faf85
Denis Frolov <d.fr8938@gmail.com>
2020-08-17 19:31:32 +0300

    add ft_putnbr

---
+++ Practice/C_00/ex07/includes/ft_h.h
@@ -0,0 +1,14 @@
+#ifndef FT_H
+# define FT_H
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_print_alphabet(void);
+void	ft_print_reverse_alphabet(void);
+void	ft_print_numbers(void);
+void	ft_is_negative(int n);
+void	ft_print_comb(void);
+void	ft_print_comb2(void);
+void	ft_putnbr(int nb);
+#endif
---
+++ Practice/C_00/ex07/main.c
@@ -0,0 +1,11 @@
+#include "ft_h.h"
+
+int	main(void)
+{
+	char slash_n;
+
+	slash_n = '\n';
+	ft_putnbr(-2147483647);
+	ft_putchar(slash_n);
+	return 0;
+}
