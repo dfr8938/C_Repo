b8f57f30cec911bc104aeb131820f759024d00d0
Denis Frolov <d.fr8938@gmail.com>
2020-08-19 22:00:12 +0300

    add ft_str_is_lowercase

---
+++ Practice/C_02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,25 @@
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
+
+int	ft_str_is_lowercase(char *str)
+{
+	int i;
+
+	i = 0;
+	if (ft_strlen(str) == 0)
+		return 1;
+	while (str[i])
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			return 1;
+		return 0;
+		i++;
+	}
+}
