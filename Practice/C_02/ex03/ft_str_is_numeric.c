a2a58bbacd433f59160b83f09c40d1d8dac35cc0
Denis Frolov <d.fr8938@gmail.com>
2020-08-19 21:53:45 +0300

    add ft_str_is_numeric

---
+++ Practice/C_02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,24 @@
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return i;
+}
+
+int	ft_str_is_numeric(char *str)
+{
+	int i;
+
+	i = 0;
+	if (ft_strlen(str) == 0)
+		return 1;
+	while (str[i])
+	{
+		if (!(str[i] >= 48 && str[i] <= 57))
+			return 0;
+		return 1;
+	}
+}
